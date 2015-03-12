0df272602411d5f1becf62d8e3bfbebfbfedf88b
Danesh Petigara <dpetigara@broadcom.com>
2015-03-12 16:25:57 -0700

    mm: cma: fix CMA aligned offset calculation
    commit 850fc430f47aad52092deaaeb32b99f97f0e6aca upstream.
    
    The CMA aligned offset calculation is incorrect for non-zero order_per_bit
    values.
    
    For example, if cma->order_per_bit=1, cma->base_pfn= 0x2f800000 and
    align_order=12, the function returns a value of 0x17c00 instead of 0x400.
    
    This patch fixes the CMA aligned offset calculation.
    
    The previous calculation was wrong and would return too-large values for
    the offset, so that when cma_alloc looks for free pages in the bitmap with
    the requested alignment > order_per_bit, it starts too far into the bitmap
    and so CMA allocations will fail despite there actually being plenty of
    free pages remaining.  It will also probably have the wrong alignment.
    With this change, we will get the correct offset into the bitmap.
    
    One affected user is powerpc KVM, which has kvm_cma->order_per_bit set to
    KVM_CMA_CHUNK_ORDER - PAGE_SHIFT, or 18 - 12 = 6.
    
    [gregory.0xf0@gmail.com: changelog additions]
    Signed-off-by: Danesh Petigara <dpetigara@broadcom.com>
    Reviewed-by: Gregory Fong <gregory.0xf0@gmail.com>
    Acked-by: Michal Nazarewicz <mina86@mina86.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/cma.c
+++ mm/cma.c
@@ -62,6 +62,24 @@ static unsigned long cma_bitmap_aligned_mask(struct cma *cma, int align_order)
 	return (1UL << (align_order - cma->order_per_bit)) - 1;
 }
 
+/*
+ * Find a PFN aligned to the specified order and return an offset represented in
+ * order_per_bits.
+ */
+static unsigned long cma_bitmap_aligned_offset(struct cma *cma, int align_order)
+{
+	if (align_order <= cma->order_per_bit)
+		return 0;
+
+	return (ALIGN(cma->base_pfn, (1UL << align_order))
+		- cma->base_pfn) >> cma->order_per_bit;
+}
+
+static unsigned long cma_bitmap_maxno(struct cma *cma)
+{
+	return cma->count >> cma->order_per_bit;
+}
+
 static unsigned long cma_bitmap_pages_to_bits(struct cma *cma,
 						unsigned long pages)
 {
